eee82ca0caea1683e541e0ab28797a30b11e9290
953034206@qq.com <DoraemonBestNo1>
2019-04-11 10:57:49 +0800

    t

--- src/main/java/com/douglei/instances/logger/LoggerHolder.java
+++ src/main/java/com/douglei/instances/logger/LoggerHolder.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 
+import com.douglei.utils.ExceptionUtil;
+
 /**
  * 日志持有对象
  * <p>对Logger对象的封装</p>
@@ -15,6 +17,19 @@ public class LoggerHolder {
 		this.logger = LoggerFactory.getLogger(clazz);
 	}
 
+	/**
+	 * 获取Throwable的详细信息
+	 * <p>如果参数是Throwable类型，则返回对应Throwable的详细信息</p>
+	 * @param arg
+	 * @return
+	 */
+	private Object getThrowableDetailMessage(Object arg) {
+		if(arg instanceof Throwable) {
+			return ExceptionUtil.getExceptionDetailMessage((Throwable)arg);
+		}
+		return arg;
+	}
+	
 	public String getName() {
 		return logger.getName();
 	}
@@ -38,18 +53,26 @@ public class LoggerHolder {
 
 	public void trace(String format, Object arg) {
 		if(logger.isTraceEnabled()){
+			arg = getThrowableDetailMessage(arg);
 			logger.trace(format, arg);
 		}
 	}
 
 	public void trace(String format, Object arg1, Object arg2) {
 		if(logger.isTraceEnabled()){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.trace(format, arg1, arg2);
 		}
 	}
 
 	public void trace(String format, Object... args) {
 		if(logger.isTraceEnabled()){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.trace(format, args);
 		}
 	}
@@ -68,18 +91,26 @@ public class LoggerHolder {
 
 	public void trace(Marker marker, String format, Object arg) {
 		if(logger.isTraceEnabled(marker)){
+			arg = getThrowableDetailMessage(arg);
 			logger.trace(marker, format, arg);
 		}
 	}
 
 	public void trace(Marker marker, String format, Object arg1, Object arg2) {
 		if(logger.isTraceEnabled(marker)){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.trace(marker, format, arg1, arg2);
 		}
 	}
 
 	public void trace(Marker marker, String format, Object... args) {
 		if(logger.isTraceEnabled(marker)){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.trace(marker, format, args);
 		}
 	}
@@ -109,18 +140,26 @@ public class LoggerHolder {
 
 	public void debug(String format, Object arg) {
 		if(logger.isDebugEnabled()){
+			arg = getThrowableDetailMessage(arg);
 			logger.debug(format, arg);
 		}
 	}
 
 	public void debug(String format, Object arg1, Object arg2) {
 		if(logger.isDebugEnabled()){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.debug(format, arg1, arg2);
 		}
 	}
 
 	public void debug(String format, Object... args) {
 		if(logger.isDebugEnabled()){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.debug(format, args);
 		}
 	}
@@ -139,18 +178,26 @@ public class LoggerHolder {
 
 	public void debug(Marker marker, String format, Object arg) {
 		if(logger.isDebugEnabled(marker)){
+			arg = getThrowableDetailMessage(arg);
 			logger.debug(marker, format, arg);
 		}
 	}
 
 	public void debug(Marker marker, String format, Object arg1, Object arg2) {
 		if(logger.isDebugEnabled(marker)){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.debug(marker, format, arg1, arg2);
 		}
 	}
 
 	public void debug(Marker marker, String format, Object... args) {
 		if(logger.isDebugEnabled(marker)){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.debug(marker, format, args);
 		}
 	}
@@ -180,18 +227,26 @@ public class LoggerHolder {
 
 	public void info(String format, Object arg) {
 		if(logger.isInfoEnabled()){
+			arg = getThrowableDetailMessage(arg);
 			logger.info(format, arg);
 		}
 	}
 
 	public void info(String format, Object arg1, Object arg2) {
 		if(logger.isInfoEnabled()){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.info(format, arg1, arg2);
 		}
 	}
 
 	public void info(String format, Object... args) {
 		if(logger.isInfoEnabled()){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.info(format, args);
 		}
 	}
@@ -210,18 +265,26 @@ public class LoggerHolder {
 
 	public void info(Marker marker, String format, Object arg) {
 		if(logger.isInfoEnabled(marker)){
+			arg = getThrowableDetailMessage(arg);
 			logger.info(marker, format, arg);
 		}
 	}
 
 	public void info(Marker marker, String format, Object arg1, Object arg2) {
 		if(logger.isInfoEnabled(marker)){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.info(marker, format, arg1, arg2);
 		}
 	}
 
 	public void info(Marker marker, String format, Object... args) {
 		if(logger.isInfoEnabled(marker)){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.info(marker, format, args);
 		}
 	}
@@ -251,18 +314,26 @@ public class LoggerHolder {
 
 	public void warn(String format, Object arg) {
 		if(logger.isWarnEnabled()){
+			arg = getThrowableDetailMessage(arg);
 			logger.warn(format, arg);
 		}
 	}
 
 	public void warn(String format, Object... args) {
 		if(logger.isWarnEnabled()){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.warn(format, args);
 		}
 	}
 
 	public void warn(String format, Object arg1, Object arg2) {
 		if(logger.isWarnEnabled()){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.warn(format, arg1, arg2);
 		}
 	}
@@ -281,18 +352,26 @@ public class LoggerHolder {
 
 	public void warn(Marker marker, String format, Object arg) {
 		if(logger.isWarnEnabled(marker)){
+			arg = getThrowableDetailMessage(arg);
 			logger.warn(marker, format, arg);
 		}
 	}
 
 	public void warn(Marker marker, String format, Object arg1, Object arg2) {
 		if(logger.isWarnEnabled(marker)){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.warn(marker, format, arg1, arg2);
 		}
 	}
 
 	public void warn(Marker marker, String format, Object... args) {
 		if(logger.isWarnEnabled(marker)){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.warn(marker, format, args);
 		}
 	}
@@ -322,18 +401,26 @@ public class LoggerHolder {
 
 	public void error(String format, Object arg) {
 		if(logger.isErrorEnabled()){
+			arg = getThrowableDetailMessage(arg);
 			logger.error(format, arg);
 		}
 	}
 
 	public void error(String format, Object arg1, Object arg2) {
 		if(logger.isErrorEnabled()){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.error(format, arg1, arg2);
 		}
 	}
 
 	public void error(String format, Object... args) {
 		if(logger.isErrorEnabled()){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.error(format, args);
 		}
 	}
@@ -352,18 +439,26 @@ public class LoggerHolder {
 
 	public void error(Marker marker, String format, Object arg) {
 		if(logger.isErrorEnabled(marker)){
+			arg = getThrowableDetailMessage(arg);
 			logger.error(marker, format, arg);
 		}
 	}
 
 	public void error(Marker marker, String format, Object arg1, Object arg2) {
 		if(logger.isErrorEnabled(marker)){
+			arg1 = getThrowableDetailMessage(arg1);
+			arg2 = getThrowableDetailMessage(arg2);
 			logger.error(marker, format, arg1, arg2);
 		}
 	}
 
 	public void error(Marker marker, String format, Object... args) {
 		if(logger.isErrorEnabled(marker)){
+			if(args != null && args.length > 0) {
+				for(int i=0;i<args.length;i++) {
+					args[i] = getThrowableDetailMessage(args[i]);
+				}
+			}
 			logger.error(marker, format, args);
 		}
 	}
