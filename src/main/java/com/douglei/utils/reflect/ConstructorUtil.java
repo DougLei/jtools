397eca2225dfe34f76e78849d584c6abb49f1e8e
DougLei <953034206@qq.com>
2019-05-09 15:49:36 +0800

    t

--- src/main/java/com/douglei/utils/reflect/ConstructorUtil.java
+++ src/main/java/com/douglei/utils/reflect/ConstructorUtil.java
@@ -8,6 +8,19 @@ public class ConstructorUtil {
 	
 	/**
 	 * 创建无参构造函数实例
+	 * @param className
+	 * @return
+	 */
+	public static Object newInstance(String className) {
+		try {
+			return Class.forName(className).newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException("类["+className+"], 在调用无参构造函数, 创建其实例时出现异常", e);
+		}
+	}
+	
+	/**
+	 * 创建无参构造函数实例
 	 * @param clz
 	 * @return
 	 */
@@ -15,7 +28,7 @@ public class ConstructorUtil {
 		try {
 			return clz.newInstance();
 		} catch (Exception e) {
-			throw new RuntimeException("根据类["+clz.getName()+"]的无参构造函数, new实例时, 出现异常", e);
+			throw new RuntimeException("类["+clz.getName()+"], 在调用无参构造函数, 创建其实例时出现异常", e);
 		}
 	}
 }
