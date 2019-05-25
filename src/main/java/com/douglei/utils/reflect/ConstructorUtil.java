230f172e56388050bac7f2154310ca9ba9fe478e
DougLei <953034206@qq.com>
2019-05-25 18:14:45 +0800

    t

--- src/main/java/com/douglei/utils/reflect/ConstructorUtil.java
+++ src/main/java/com/douglei/utils/reflect/ConstructorUtil.java
@@ -7,6 +7,23 @@ package com.douglei.utils.reflect;
 public class ConstructorUtil {
 	
 	/**
+	 * 判断类是否存在
+	 * @param className
+	 * @return
+	 */
+	public static boolean classIsExists(String className) {
+		try {
+			// 在反射创建这个类的时候，不会调用到类的static块，属性，和方法，效率高于class.forName
+			// 和loadClass正好相反，在只是判断一个类是否存在的情况下，不需要执行类的static块，属性，和方法，以提高效率
+			Thread.currentThread().getContextClassLoader().loadClass(className);
+//			Class.forName(classpath);// 在创建类的实例的时候，会调用类的static块，属性，和方法
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+	}
+	
+	/**
 	 * 创建无参构造函数实例
 	 * @param className
 	 * @return
