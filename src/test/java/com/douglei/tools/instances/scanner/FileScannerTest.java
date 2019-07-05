389c093a3f537a3e83c2dc8bf0b89331c356aa8c
DougLei <953034206@qq.com>
2019-07-05 17:27:23 +0800

    t

--- src/test/java/com/douglei/tools/instances/scanner/FileScannerTest.java
+++ src/test/java/com/douglei/tools/instances/scanner/FileScannerTest.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.douglei.tools.instances.scanner.FileScanner;
-
 public class FileScannerTest {
 	
 	@Test
 	public void scan(){
 		String basePath = "com/douglei/tools/instances/scanner";
-		List<String> allFileNames = new FileScanner("class").scan(basePath);
+		List<String> allFileNames = new FileScanner("xml").scan(basePath);
 		System.out.println(allFileNames.size());
 		for (String fileName : allFileNames) {
 			System.out.println(fileName);
