e12b79fbb41ae5401b1637a63f2d89e366bfe3b6
DougLei <953034206@qq.com>
2019-07-12 11:03:44 +0800

    t

---
+++ src/main/java/com/douglei/tools/utils/reflect/ValidationUtil.java
@@ -0,0 +1,25 @@
+package com.douglei.tools.utils.reflect;
+
+/**
+ * 验证工具类
+ * @author DougLei
+ */
+public class ValidationUtil {
+	
+	/**
+	 * 是否实现了targetInterface
+	 * @param targetInterface
+	 * @param interfaces
+	 * @return
+	 */
+	public static boolean isImplementInterface(Class<?> targetInterface, Class<?>[] interfaces) {
+		if(interfaces.length > 0) {
+			for (Class<?> interface_ : interfaces) {
+				if(interface_ == targetInterface) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
