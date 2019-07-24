27e3a4d19195da00aa3bcb491e3f317eed5a0ad1
DougLei <953034206@qq.com>
2019-07-24 19:41:05 +0800

    t

---
+++ src/main/java/com/douglei/tools/ToolsException.java
@@ -0,0 +1,29 @@
+package com.douglei.tools;
+
+/**
+ * 所有异常的父类
+ * @author DougLei
+ */
+public class ToolsException extends RuntimeException {
+	private static final long serialVersionUID = 6853162634170660728L;
+
+	public ToolsException() {
+		super();
+	}
+
+	public ToolsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ToolsException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ToolsException(String message) {
+		super(message);
+	}
+
+	public ToolsException(Throwable cause) {
+		super(cause);
+	}
+}
