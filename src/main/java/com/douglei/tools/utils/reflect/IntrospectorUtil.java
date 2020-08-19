c76898c5dd53ab43e9b4d2e0763caa7a5b661cff
DougLei <953034206@qq.com>
2020-08-19 17:32:00 +0800

    t

--- src/main/java/com/douglei/tools/utils/reflect/IntrospectorUtil.java
+++ src/main/java/com/douglei/tools/utils/reflect/IntrospectorUtil.java
@@ -7,12 +7,12 @@ import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.douglei.tools.utils.CollectionUtil;
-import com.douglei.tools.utils.StringUtil;
 import com.douglei.tools.utils.UtilException;
 import com.douglei.tools.utils.datatype.converter.ConverterUtil;
 
@@ -25,23 +25,19 @@ public class IntrospectorUtil {
 	private static final Logger logger = LoggerFactory.getLogger(IntrospectorUtil.class);
 	
 	/**
-	 * 通过内省, 获取对象指定的属性值
-	 * @param introspectorObject
+	 * 通过内省, 获取对象指定的属性值, 如果传入的 object是Map, 则直接使用get方法
+	 * @param object
 	 * @param propertyName
 	 * @return
 	 */
-	public static Object getProperyValue(Object introspectorObject, String propertyName) {
-		if(introspectorObject == null) {
-			throw new NullPointerException("getProperyValue时, 传入的introspectorObject实例=null");
-		}
-		if(StringUtil.notEmpty(propertyName)) {
-			Class<?> introspectorClass = introspectorObject.getClass();
-			if(logger.isDebugEnabled()) {
-				logger.debug("获取[{}]实例, {} 属性值", introspectorClass, propertyName);
-			}
-			
+	@SuppressWarnings("rawtypes")
+	public static Object getProperyValue(Object object, String propertyName) {
+		if(object instanceof Map) {
+			return ((Map)object).get(propertyName);
+		}else {
+			Class<?> clz = object.getClass();
 			try {
-				BeanInfo beanInfo = Introspector.getBeanInfo(introspectorClass);
+				BeanInfo beanInfo = Introspector.getBeanInfo(clz);
 				PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
 				
 				Method getter = null;
@@ -49,44 +45,45 @@ public class IntrospectorUtil {
 					if(pd.getName().equals(propertyName)) {
 						getter = pd.getReadMethod();
 						if(getter == null) {
-							throw new NullPointerException("无法调用 "+introspectorClass+"."+toGetMethodName(propertyName)+"方法, 程序没有获取到该方法");
+							throw new NullPointerException("无法调用 "+clz+"."+toGetMethodName(propertyName)+"方法, 程序没有获取到该方法");
 						}
 						if(logger.isDebugEnabled()) {
-							logger.debug("调用 {}.{}, get出的参数值为 {}, 值类型为 {}", introspectorClass, getter.getName(), propertyName, pd.getPropertyType());
+							logger.debug("调用 {}.{}, get出的参数值为 {}, 值类型为 {}", clz, getter.getName(), propertyName, pd.getPropertyType());
 						}
-						return getter.invoke(introspectorObject);
+						return getter.invoke(object);
 					}
 				}
 			} catch (Exception e) {
-				throw new UtilException("["+introspectorClass+"]实例, 调用 "+toGetMethodName(propertyName)+"() 方法时, 出现异常", e);
+				throw new UtilException("["+clz+"]实例, 调用 "+toGetMethodName(propertyName)+"() 方法时, 出现异常", e);
 			}
 		}
 		return null;
 	}
+	
 	/**
-	 * 通过内省, 获取对象的属性值集合
-	 * @param introspectorObject
+	 * 通过内省, 获取对象的属性值集合, 如果传入的 object是Map, 则直接使用get方法
+	 * @param object
 	 * @param propertyNames
 	 * @return
 	 */
-	public static Map<String, Object> getProperyValues(Object introspectorObject, Collection<String> propertyNames) {
-		if(introspectorObject == null) {
-			throw new NullPointerException("getProperyValues时, 传入的introspectorObject实例=null");
-		}
-		if(CollectionUtil.unEmpty(propertyNames)) {
-			Class<?> introspectorClass = introspectorObject.getClass();
-			if(logger.isDebugEnabled()) {
-				logger.debug("获取[{}]实例, {} 属性值集合", introspectorClass, propertyNames);
+	@SuppressWarnings("rawtypes")
+	public static Map<String, Object> getProperyValues(Object object, Collection<String> propertyNames) {
+		Map<String, Object> valueMap = new HashMap<String, Object>();
+		
+		if(object instanceof Map) {
+			Map map = ((Map)object);
+			for (String pn : propertyNames) {
+				valueMap.put(pn, map.get(pn));
 			}
-			
+			return valueMap;
+		}else {
+			Class<?> clz = object.getClass();
 			String propertyName = null;
 			try {
-				BeanInfo beanInfo = Introspector.getBeanInfo(introspectorClass);
+				BeanInfo beanInfo = Introspector.getBeanInfo(clz);
 				PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
 				
 				int size = propertyNames.size();
-				Map<String, Object> valueMap = new HashMap<String, Object>(size);
-				
 				int index = 0;
 				Method getter = null;
 				for (PropertyDescriptor pd : pds) {
@@ -94,12 +91,12 @@ public class IntrospectorUtil {
 					if(propertyNames.contains(propertyName)) {
 						getter = pd.getReadMethod();
 						if(getter == null) {
-							throw new NullPointerException("无法调用 "+introspectorClass+"."+toGetMethodName(propertyName)+"方法, 程序没有获取到该方法");
+							throw new NullPointerException("无法调用 "+clz+"."+toGetMethodName(propertyName)+"方法, 程序没有获取到该方法");
 						}
-						valueMap.put(propertyName, getter.invoke(introspectorObject));
+						valueMap.put(propertyName, getter.invoke(object));
 						
 						if(logger.isDebugEnabled()) {
-							logger.debug("调用 {}.{}, get出的参数值为 {}, 值类型为 {}", introspectorClass, getter.getName(), valueMap.get(propertyName), pd.getPropertyType());
+							logger.debug("调用 {}.{}, get出的参数值为 {}, 值类型为 {}", clz, getter.getName(), valueMap.get(propertyName), pd.getPropertyType());
 						}
 						index++;
 						if(index == size) {
@@ -109,108 +106,108 @@ public class IntrospectorUtil {
 				}
 				return valueMap;
 			} catch (Exception e) {
-				throw new UtilException("["+introspectorClass+"]实例, 调用 "+toGetMethodName(propertyName)+"() 方法时, 出现异常", e);
+				throw new UtilException("["+clz+"]实例, 调用 "+toGetMethodName(propertyName)+"() 方法时, 出现异常", e);
 			}
 		}
-		return null;
 	}
 	private static String toGetMethodName(String fieldName) {
 		return "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
 	}
 	
 	/**
-	 * 通过内省, 给对象的属性赋值
-	 * @param introspectorObject
+	 * 通过内省, 给对象的属性赋值, 如果传入的 object是Map, 则直接使用set方法
+	 * @param object
 	 * @param propertyName
 	 * @param propertyValue
 	 * @return
 	 */
-	public static Object setProperyValue(Object introspectorObject, String propertyName, Object propertyValue) {
-		if(introspectorObject == null) {
-			throw new NullPointerException("setProperyValue时, 传入的introspectorObject实例=null");
-		}
-		if(StringUtil.notEmpty(propertyName) && propertyValue != null) {
-			Class<?> introspectorClass = introspectorObject.getClass();
-			if(logger.isDebugEnabled()) {
-				logger.debug("给[{}]实例, set {} 属性值 {}", introspectorClass, propertyName, propertyValue);
-			}
-			
-			try {
-				BeanInfo beanInfo = Introspector.getBeanInfo(introspectorClass);
-				PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
-				
-				Method setter;
-				for (PropertyDescriptor pd : pds) {
-					if(pd.getName().equals(propertyName)) {
-						setter = pd.getWriteMethod();
-						if(setter == null) {
-							throw new NullPointerException("无法调用 "+introspectorClass+"."+toSetMethodName(propertyName)+"() 方法, 程序没有获取到该方法");
-						}
-						if(logger.isDebugEnabled()) {
-							logger.debug("调用 {}.{}, set进的参数值为 {}, 值类型为 {}", introspectorClass, setter.getName(), propertyValue, pd.getPropertyType());
-						}
-						setter.invoke(introspectorObject, ConverterUtil.convert(propertyValue, pd.getPropertyType()));
-						break;
-					}
-				}
-			} catch (Exception e) {
-				throw new UtilException("["+introspectorClass+"]实例, 调用 "+toSetMethodName(propertyName)+"() 方法时, 出现异常", e);
-			}
-		}
-		return introspectorObject;
-	}
-	/**
-	 * 通过内省, 给对象的属性赋值
-	 * @param introspectorObject
-	 * @param propertyMap
-	 * @return
-	 */
-	public static Object setProperyValues(Object introspectorObject, Map<String, ? extends Object> propertyMap) {
-		if(introspectorObject == null) {
-			throw new NullPointerException("setProperyValues时, 传入的introspectorObject实例=null");
-		}
-		if(CollectionUtil.unEmpty(propertyMap)) {
-			Class<?> introspectorClass = introspectorObject.getClass();
-			if(logger.isDebugEnabled()) {
-				logger.debug("给[{}]实例, set {} 属性值map集合", introspectorClass, propertyMap);
-			}
-			
-			String propertyName = null;
-			try {
-				BeanInfo beanInfo = Introspector.getBeanInfo(introspectorClass);
-				PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
-				
-				int size = propertyMap.size();
-				int index = 0;
-				Object value = null;
-				Method setter = null;
-				for (PropertyDescriptor pd : pds) {
-					propertyName = pd.getName();
-					if(propertyMap.containsKey(propertyName)) {
-						index++;
-						
-						value = propertyMap.get(propertyName);
-						if(value != null) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Object setProperyValue(Object object, String propertyName, Object propertyValue) {
+		if(propertyValue != null) {
+			if(object instanceof Map) {
+				((Map)object).put(propertyName, propertyValue);
+			}else {
+				Class<?> clz = object.getClass();
+				try {
+					BeanInfo beanInfo = Introspector.getBeanInfo(clz);
+					PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+					
+					Method setter;
+					for (PropertyDescriptor pd : pds) {
+						if(pd.getName().equals(propertyName)) {
 							setter = pd.getWriteMethod();
 							if(setter == null) {
-								throw new NullPointerException("无法调用 "+introspectorClass+"."+toSetMethodName(propertyName)+"() 方法, 程序没有获取到该方法");
+								throw new NullPointerException("无法调用 "+clz+"."+toSetMethodName(propertyName)+"() 方法, 程序没有获取到该方法");
 							}
-							setter.invoke(introspectorObject, ConverterUtil.convert(value, pd.getPropertyType()));
-							
 							if(logger.isDebugEnabled()) {
-								logger.debug("调用 {}.{}, set进的参数值为 {}, 值类型为 {}", introspectorClass, setter.getName(), value, pd.getPropertyType());
+								logger.debug("调用 {}.{}, set进的参数值为 {}, 值类型为 {}", clz, setter.getName(), propertyValue, pd.getPropertyType());
 							}
-						}
-						if(index == size) {
+							setter.invoke(object, ConverterUtil.convert(propertyValue, pd.getPropertyType()));
 							break;
 						}
 					}
+				} catch (Exception e) {
+					throw new UtilException("["+clz+"]实例, 调用 "+toSetMethodName(propertyName)+"() 方法时, 出现异常", e);
 				}
-			} catch (Exception e) {
-				throw new UtilException("["+introspectorClass+"]实例, 调用 "+toSetMethodName(propertyName)+"() 方法时, 出现异常", e);
 			}
 		}
-		return introspectorObject;
+		return object;
+	}
+	
+	/**
+	 * 通过内省, 给对象的属性赋值, 如果传入的 object是Map, 则直接使用set方法
+	 * @param object
+	 * @param propertyMap
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Object setProperyValues(Object object, Map<String, ? extends Object> propertyMap) {
+		if(CollectionUtil.unEmpty(propertyMap)) {
+			if(object instanceof Map) {
+				Map map = ((Map)object);
+				for(Entry<String, ? extends Object> pv : propertyMap.entrySet()) {
+					if(pv.getValue() != null)
+						map.put(pv.getKey(), pv.getValue());
+				}
+			}else {
+				Class<?> clz = object.getClass();
+				String propertyName = null;
+				try {
+					BeanInfo beanInfo = Introspector.getBeanInfo(clz);
+					PropertyDescriptor[] pds = beanInfo.getPropertyDescriptors();
+					
+					int size = propertyMap.size();
+					int index = 0;
+					Object value = null;
+					Method setter = null;
+					for (PropertyDescriptor pd : pds) {
+						propertyName = pd.getName();
+						if(propertyMap.containsKey(propertyName)) {
+							index++;
+							
+							value = propertyMap.get(propertyName);
+							if(value != null) {
+								setter = pd.getWriteMethod();
+								if(setter == null) {
+									throw new NullPointerException("无法调用 "+clz+"."+toSetMethodName(propertyName)+"() 方法, 程序没有获取到该方法");
+								}
+								setter.invoke(object, ConverterUtil.convert(value, pd.getPropertyType()));
+								
+								if(logger.isDebugEnabled()) {
+									logger.debug("调用 {}.{}, set进的参数值为 {}, 值类型为 {}", clz, setter.getName(), value, pd.getPropertyType());
+								}
+							}
+							if(index == size) {
+								break;
+							}
+						}
+					}
+				} catch (Exception e) {
+					throw new UtilException("["+clz+"]实例, 调用 "+toSetMethodName(propertyName)+"() 方法时, 出现异常", e);
+				}
+			}
+		}
+		return object;
 	}
 	private static String toSetMethodName(String fieldName) {
 		return "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
