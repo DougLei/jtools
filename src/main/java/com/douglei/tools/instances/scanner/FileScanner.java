4c4ac40a51745db77fd26eed8e936509d8eb29a4
DougLei <953034206@qq.com>
2020-08-28 14:28:10 +0800

    t

--- src/main/java/com/douglei/tools/instances/scanner/FileScanner.java
+++ src/main/java/com/douglei/tools/instances/scanner/FileScanner.java
@@ -135,7 +135,7 @@ public class FileScanner extends Scanner{
 		InputStream in = null;
 		try {
 			if(path.startsWith(JAR_FILE_PREFIX)) {
-				in = fileScannerClassLoader.getResourceAsStream(path.substring(JAR_FILE_PREFIX.length()));
+				in = FileScanner.class.getClassLoader().getResourceAsStream(path.substring(JAR_FILE_PREFIX.length()));
 				if(in == null) {
 					throw new NullPointerException();
 				}
@@ -147,5 +147,4 @@ public class FileScanner extends Scanner{
 			throw new ScannerException("给定的["+path+"], 不存在任何文件");
 		}
 	}
-	private static final ClassLoader fileScannerClassLoader = FileScanner.class.getClassLoader();
 }
